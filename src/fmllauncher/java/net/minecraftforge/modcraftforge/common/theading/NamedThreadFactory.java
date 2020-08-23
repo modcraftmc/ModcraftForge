16d5713f1defbacb7bd2fd7ea54e5796d668de9d
manugame <manugameur.pro@gmail.com>
2020-08-23 14:10:47 +0200

    improve player login thread

---
+++ src/fmllauncher/java/net/minecraftforge/modcraftforge/common/theading/NamedThreadFactory.java
@@ -0,0 +1,24 @@
+package net.minecraftforge.modcraftforge.common.theading;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.concurrent.ThreadFactory;
+
+public class NamedThreadFactory implements ThreadFactory {
+
+    private static int id = 0;
+    private String name;
+
+    public NamedThreadFactory(String name) {
+        this.name = name;
+
+    }
+
+
+    @Override
+    public Thread newThread(@NotNull Runnable r) {
+        Thread thread = new ModcraftThreadBox.AssignableThread(r);
+        thread.setName(name + "#" + id++);
+        return thread;
+    }
+}
