cbc9ba6c4ba0d37acd8563556188b40ba2e385f5
manugame <manugameur.pro@gmail.com>
2020-12-17 17:35:40 +0100

    From Yatopia - lithium patches - 1/2

---
+++ src/main/java/org/yatopiamc/yatopia/server/util/HoldingConsumer.java
@@ -0,0 +1,17 @@
+package org.yatopiamc.yatopia.server.util;
+
+import java.util.function.Consumer;
+
+public class HoldingConsumer<T> implements Consumer<T> {
+
+    private T value;
+
+    @Override
+    public void accept(T t) {
+        this.value = t;
+    }
+
+    public T getValue() {
+        return value;
+    }
+}
